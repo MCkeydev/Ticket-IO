3b5644463a637842c003aab062011b9cc86e380a
LAPTOP_MATTHIEU\mattc <mattcourrejou@gmail.com>
2022-09-27 11:55:36 +0200

    [+][UPD][Database] Création de la relation entre user et service.

--- src/Entity/Service.php
+++ src/Entity/Service.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ServiceRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ServiceRepository::class)]
@@ -15,6 +17,14 @@ class Service
 
     #[ORM\Column(length: 255)]
     private ?string $nom = null;
+
+    #[ORM\OneToMany(mappedBy: 'service', targetEntity: User::class)]
+    private Collection $membres;
+
+    public function __construct()
+    {
+        $this->membres = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +39,36 @@ class Service
     public function setNom(string $nom): self
     {
         $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, User>
+     */
+    public function getMembres(): Collection
+    {
+        return $this->membres;
+    }
+
+    public function addMembre(User $membre): self
+    {
+        if (!$this->membres->contains($membre)) {
+            $this->membres->add($membre);
+            $membre->setService($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMembre(User $membre): self
+    {
+        if ($this->membres->removeElement($membre)) {
+            // set the owning side to null (unless already changed)
+            if ($membre->getService() === $this) {
+                $membre->setService(null);
+            }
+        }
 
         return $this;
     }
