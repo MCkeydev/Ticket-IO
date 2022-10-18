3de3f9a6ceed158091a38f54d5df8c61f84b5f24
Matthieu Courréjou <mattcourrejou@gmail.com>
2022-10-18 15:20:40 +0200

    éer_utilisateue: Auto stash before rebase of "origin/Development"

--- src/Controller/TicketController.php
+++ src/Controller/TicketController.php
@@ -24,31 +24,6 @@ class TicketController extends AbstractController
     #[Route('/ticket/create', name: 'app_ticket_create', methods: ['get','post'])]
     public function createTicket(ManagerRegistry $registre, SerializerInterface $serializer, Request $request): Response
     {
-            // Récupération des objets depuis la BDD
-        $service = $registre->getRepository(Service::class)->findOneBy(['nom'=>$request->get('service')]);
-        $operateur = $registre->getRepository(Operateur::class)->find(1);
-        $user = $registre->getRepository(User::class)->findOneBy(['email'=>$request->get('client')]);
-        $criticite = $registre->getRepository(Criticite::class)->findOneBy(['libelle'=>$request->get('criticite')]);
-        $gravite = $registre->getRepository(Gravite::class)->findOneBy(['libelle'=>$request->get('gravite')]);
-        $status = $registre->getRepository(Status::class)->findOneBy(['libelle'=>$request->get('status')]);
-        
-            // Création du Ticket avec les différents objets récupérés.
-        $ticket= new Ticket();
-        $ticket->setTitre('Ticket1')
-        ->setDescription('ticket de test')
-        ->setService($service)
-        ->setOperateur($operateur)
-        ->setClient($user)
-        ->setStatus($status)
-        ->setGravite($gravite)
-        ->setCriticite($criticite);
 
-            // Envoi de l'objet dans la BDD.
-        $manager= $registre->getManager();
-        $manager->persist($ticket);
-        $manager->flush();
-         return new Response($serializer->serialize($ticket,'json',[AbstractObjectNormalizer::CIRCULAR_REFERENCE_HANDLER => function($param, $param2){
-         return $param->getId();
-         }]), Response::HTTP_OK );
     }
 }
