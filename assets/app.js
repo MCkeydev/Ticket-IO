94351d81f8331d063c12f88a4f8bd890222ab7a3
Matthieu Courréjou <mattcourrejou@gmail.com>
2022-12-03 19:44:20 +0100

    Auto stash before merge of "create/solution" and "origin/solution/ajout"

--- assets/app.js
+++ assets/app.js
@@ -8,10 +8,12 @@
 // any CSS you import will output into a single css file (app.scss in this case)
 import "./styles/app.scss";
 import "../templates/components/iconButton/iconButton.scss";
+import "../templates/components/avatar/avatar.scss";
 import "../templates/components/navbar/navbar.scss";
+import "../templates/components/statusChip/statusChip.scss";
 import "../templates/security/login.scss";
 import "../templates/ticket/updateTicket/updateTicket.scss";
+import "../templates/suivi/suivi.scss";
 import "../templates/accueil/accueil.scss";
-import '../templates/suivi/suivi.scss';
 // start the Stimulus application
 import "./bootstrap";
