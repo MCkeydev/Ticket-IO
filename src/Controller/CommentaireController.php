d2c205992c4b3f98da79ac874348ecd214c78ec8
DESKTOP-NTP1PFC\fabie <fabiencapoen1997@gmail.com>
2023-01-15 16:02:19 +0100

    [++][APD][Finalisation fonctionnalités] test  fonctionnalités, fix bus

--- src/Controller/CommentaireController.php
+++ src/Controller/CommentaireController.php
@@ -19,7 +19,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentaireController extends AbstractController
 {
 	use FormTrait;
-    use SuiviTrait;
+	use SuiviTrait;
 	#[
 		Route(
 			"/commentaire/create/{id}",
@@ -32,20 +32,27 @@ class CommentaireController extends AbstractController
 		EntityManagerInterface $manager,
 		Request $request
 	): Response {
-        // On récupère l'utilisateur connecté.
-        $currentUser = $this->getUser();
+		// On récupère l'utilisateur connecté.
+		$currentUser = $this->getUser();
 
-        // Nous récupérons tout le suivi du ticket en question
-        $objects = $this->getTicketSuivi($ticket);
+		// Nous récupérons tout le suivi du ticket en question
+		$objects = $this->getTicketSuivi($ticket);
 
-        /**
-         * Il n'est possible d'ajouter un commentaire que sur un ticket qui n'est pas clos,
-         * nous allons alors vérifier le status de ce dernier.
-         * Si le ticket n'appartient pas au service du technicien, il n'a pas non plus d'accès.
-         */
-        if ($currentUser->getService() !== $ticket->getService() || $ticket->getStatus()->getLibelle() === 'Clos') {
-            throw $this->createNotFoundException();
-        }
+		/**
+		 * Il n'est possible d'ajouter un commentaire que sur un ticket qui n'est pas clos,
+		 * nous allons alors vérifier le status de ce dernier.
+		 * Si le ticket n'appartient pas au service du technicien, il n'a pas non plus d'accès.
+		 */
+		if ($ticket->getStatus()->getLibelle() === "Clos") {
+			throw $this->createNotFoundException();
+		}
+		if (
+			!$currentUser instanceof Operateur &&
+			$currentUser instanceof Technicien &&
+			$currentUser->getService() !== $ticket->getService()
+		) {
+			throw $this->createNotFoundException();
+		}
 
 		// On vient créer le formulaire du commentaire, et le futur commentaire.
 		$commentaire = new Commentaire();
@@ -55,28 +62,30 @@ class CommentaireController extends AbstractController
 		if ($form->isSubmitted() && $form->isValid()) {
 			if ($currentUser instanceof Technicien) {
 				$commentaire->setTechnicien($currentUser);
-			} else if ($currentUser instanceof Operateur) {
-                $commentaire->setOperateur($currentUser);
-            }
+			} elseif ($currentUser instanceof Operateur) {
+				$commentaire->setOperateur($currentUser);
+			}
 
 			// on récupère le formulaire
 			$commentaire = $form->getData();
 
-            // Nous mettons à jour la date de dernière MAJ du ticket.
-            $ticket->setUpdatedAt(new DateTimeImmutable());
-            $commentaire->setTicket($ticket);
+			// Nous mettons à jour la date de dernière MAJ du ticket.
+			$ticket->setUpdatedAt(new DateTimeImmutable());
+			$commentaire->setTicket($ticket);
 
 			$manager->persist($commentaire);
 			$manager->flush();
 
-            return $this->redirectToRoute('app_ticket_suivi', ['id' => $ticket->getId() ]);
+			return $this->redirectToRoute("app_ticket_suivi", [
+				"id" => $ticket->getId(),
+			]);
 		}
 
-        return $this->renderForm("suivi/suiviModif/suiviModif.twig.html", [
-            'titre' => 'Ajouter un commentaire',
-            'ticket' => $ticket,
-            'objects' => $objects,
-            "form" => $form,
-        ]);
+		return $this->renderForm("suivi/suiviModif/suiviModif.twig.html", [
+			"titre" => "Ajouter un commentaire",
+			"ticket" => $ticket,
+			"objects" => $objects,
+			"form" => $form,
+		]);
 	}
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,76 +7,78 @@ use App\Repository\UserRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity("email", message: "Le compte existe déjà")]
 class User extends AbstractUserClass
 {
-    #[ORM\OneToMany(mappedBy: 'client', targetEntity: Ticket::class)]
-    private Collection $tickets;
+	#[ORM\OneToMany(mappedBy: "client", targetEntity: Ticket::class)]
+	private Collection $tickets;
 
-    #[ORM\Column(length: 255)]
-    private ?string $nom = null;
+	#[ORM\Column(length: 255)]
+	private ?string $nom = null;
 
-    #[ORM\Column(length: 255)]
-    private ?string $prenom = null;
+	#[ORM\Column(length: 255)]
+	private ?string $prenom = null;
 
-    public function __construct()
-    {
-        $this->tickets = new ArrayCollection();
-        $this->setRoles(['ROLE_USER']);
-    }
+	public function __construct()
+	{
+		$this->tickets = new ArrayCollection();
+		$this->setRoles(["ROLE_USER"]);
+	}
 
-    /**
-     * @return Collection<int, Ticket>
-     */
-    public function getTickets(): Collection
-    {
-        return $this->tickets;
-    }
+	/**
+	 * @return Collection<int, Ticket>
+	 */
+	public function getTickets(): Collection
+	{
+		return $this->tickets;
+	}
 
-    public function addTicket(Ticket $ticket): self
-    {
-        if (!$this->tickets->contains($ticket)) {
-            $this->tickets->add($ticket);
-            $ticket->setClient($this);
-        }
+	public function addTicket(Ticket $ticket): self
+	{
+		if (!$this->tickets->contains($ticket)) {
+			$this->tickets->add($ticket);
+			$ticket->setClient($this);
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function removeTicket(Ticket $ticket): self
-    {
-        if ($this->tickets->removeElement($ticket)) {
-            // set the owning side to null (unless already changed)
-            if ($ticket->getClient() === $this) {
-                $ticket->setClient(null);
-            }
-        }
+	public function removeTicket(Ticket $ticket): self
+	{
+		if ($this->tickets->removeElement($ticket)) {
+			// set the owning side to null (unless already changed)
+			if ($ticket->getClient() === $this) {
+				$ticket->setClient(null);
+			}
+		}
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getNom(): ?string
-    {
-        return $this->nom;
-    }
+	public function getNom(): ?string
+	{
+		return $this->nom;
+	}
 
-    public function setNom(string $nom): self
-    {
-        $this->nom = $nom;
+	public function setNom(string $nom): self
+	{
+		$this->nom = $nom;
 
-        return $this;
-    }
+		return $this;
+	}
 
-    public function getPrenom(): ?string
-    {
-        return $this->prenom;
-    }
+	public function getPrenom(): ?string
+	{
+		return $this->prenom;
+	}
 
-    public function setPrenom(string $prenom): self
-    {
-        $this->prenom = $prenom;
+	public function setPrenom(string $prenom): self
+	{
+		$this->prenom = $prenom;
 
-        return $this;
-    }
+		return $this;
+	}
 }
